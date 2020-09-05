217a5d15a18cc5ded34d73b5bde793594475487c
MahdiSml <mahdisml98@gmail.com>
2020-09-05 15:37:01 +0430

    Compiler 2nd Stpe

--- compiler.dart
+++ compiler.dart
@@ -1,17 +1,21 @@
 // DartGodot Compiler (Dart2Godot)
-// it must find all dart files in script folder +
-// and compile it
-// and recorrect it
+// recorrect it
 // and to compiled (runnable c#)
 
 import 'dart:io';
+import 'package:process_run/process_run.dart';
 
-void main (){
+Future<void> main () async {
   
   var scriptsFolder = Directory('scripts');
-  List scripts = scriptsFolder.listSync();
-  scripts.forEach((f) {
-    print(f);
+  var scripts = scriptsFolder.listSync();
+  scripts.forEach((f) async {
+    var input = f.path;
+    var destination = 'compiled\\${f.path.split('\\')[1].split('.')[0]}.d2g';
+    await run('dart2js',['-O4','-o',destination,input], verbose: true);
+    await File(destination+'.deps').delete();
+    await File(destination+'.map').delete();
   });
+  
 
 }
