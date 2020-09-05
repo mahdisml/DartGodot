7fb9173e9ae25ae599e02d977c0d25c06f4eaa59
MahdiSml <mahdisml98@gmail.com>
2020-09-06 03:56:02 +0430

    Compiler 3rd Step

--- compiler.dart
+++ compiler.dart
@@ -1,5 +1,4 @@
 // DartGodot Compiler (Dart2Godot)
-// recorrect it
 // and to compiled (runnable c#)
 
 import 'dart:io';
@@ -11,11 +10,36 @@ Future<void> main () async {
   var scripts = scriptsFolder.listSync();
   scripts.forEach((f) async {
     var input = f.path;
-    var destination = 'compiled\\${f.path.split('\\')[1].split('.')[0]}.d2g';
+    var destination = 'compiled\\data\\${f.path.split('\\')[1].split('.')[0]}.d2g';
     await run('dart2js',['-O4','-o',destination,input], verbose: true);
+
+    var data = await File(destination).readAsString();
+    await File(destination).delete();
     await File(destination+'.deps').delete();
     await File(destination+'.map').delete();
+
+    String replaceCharAt(String oldString, int index, String newChar) {
+      return oldString.substring(0, index) + newChar + oldString.substring(index + 1);
+    }
+    var length = data.length;
+    for ( var i = 0 ; i + 4 < length ; i++){
+      if (data [i] == 's' && data [i+1] == 'e' &&
+          data [i+2] == 'l' && data [i+3] == 'f' && data [i+4] == '.'){
+        data = replaceCharAt (data,i,'');
+        data = replaceCharAt (data,i,'');
+        data = replaceCharAt (data,i,'');
+        data = replaceCharAt (data,i,'');
+        data = replaceCharAt (data,i,'');
+      }
+      length = data.length;
+    }
+    final file = await File(destination);
+    await file.writeAsString(data);
+
+    var csDestination = 'compiled\\${f.path.split('\\')[1].split('.')[0]}.cs';
+
   });
+
   
 
 }
